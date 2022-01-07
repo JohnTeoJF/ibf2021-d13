d82d6804900c349dc944c0004444bd09e5d49261
JohnTeoJF <johnteojf+github@gmail.com>
2022-01-07 14:02:06 +0800

    New

---
+++ src/main/java/com/example/ibf2021d13/Ibf2021D13Application.java
@@ -0,0 +1,13 @@
+package com.example.ibf2021d13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ibf2021D13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ibf2021D13Application.class, args);
+	}
+
+}
